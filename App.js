895cd579f623828eac6459fc5bf64f4c373f1e48
Ethan van Heerden <ethan@vpoint.biz>
2024-03-18 23:29:13 -0400

    fix app setup

--- App.js
+++ App.js
@@ -1,4 +1,12 @@
-const express = require('express')
+import express from "express";
+import cors from "cors";
+
+
 const app = express()
+app.use(cors());
+app.use(express.json());
+
 app.get('/hello', (req, res) => {res.send('Hello World!')})
-app.listen(4000)
+app.listen(process.env.PORT || 4000);
+
+
