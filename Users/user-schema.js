754a39332dc0b2662789e6c10a706f19a6ba329e
Ethan van Heerden <ethan@vpoint.biz>
2024-03-27 20:16:18 -0400

    work

---
+++ Users/user-schema.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: String, // Firebase-given UID
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dob: { type: Date, required: true },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+    stravaId: { type: String, required: false, default: "" },
+  },
+  { collection: "users" }
+);
+
+export default userSchema;
