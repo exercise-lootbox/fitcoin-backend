2e5268f8c1974b77cd4a7d980313f2317995cac9
snavemail <liam.jk.evans@gmail.com>
2024-04-23 13:35:02 -0400

    Add items attribute to user and add routes for buying item

--- Users/user-dao.js
+++ Users/user-dao.js
@@ -14,6 +14,34 @@ export const deleteUser = (userId) => userModel.deleteOne({ _id: userId });
 export const addStravaId = (userId, stravaId) =>
   userModel.updateOne({ _id: userId }, { $set: { stravaId } });
 
+// Helper for buyItem
+const addItemId = async (userId, itemId) => {
+  await userModel.updateOne({ _id: userId }, { $push: { items: itemId } });
+};
+
+// Helper for buyItem
+const removeCoins = async (userId, coins) => {
+  await userModel.updateOne({ _id: userId }, { $inc: { coins: -coins } });
+};
+
+export const buyItem = async (userId, itemId, coins) => {
+  const user = await userModel.findOne({ _id: userId });
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  if (user.coins < coins) {
+    throw new Error("Not enough coins to buy the item");
+  }
+
+  if (!itemId || !coins) {
+    throw new Error("Item ID and coins are required");
+  }
+
+  await addItemId(userId, itemId);
+  await removeCoins(userId, coins);
+};
+
 export const getCoins = async (userId) => {
   const user = await findUserById(userId);
   return user.coins;
