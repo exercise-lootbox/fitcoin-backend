7fcc1efbb33341f7eb5620ec50720db4e880577a
snavemail <liam.jk.evans@gmail.com>
2024-04-23 13:34:04 -0400

    Remove virtual schema

--- Item/item-schema.js
+++ Item/item-schema.js
@@ -21,36 +21,4 @@ const itemSchema = new mongoose.Schema(
   },
 );
 
-itemSchema.virtual("percentage").get(function () {
-  const { rarity, lootbox_id } = this;
-  const percentageMap = {
-    COMMON: 0.6,
-    UNCOMMON: 0.3,
-    EPIC: 0.08,
-    LEGENDARY: 0.02,
-  };
-  // const totalRareItems = await lootboxDao.getItemsByRarity(lootbox_id, rarity);
-  return percentageMap[rarity.toUpperCase()] || 0;
-});
-
-itemSchema.virtual("sellPrice").get(function () {
-  const { rarity, lootbox_id } = this;
-
-  // const lootbox = lootboxDao.findLootboxById(lootbox_id);
-  // console.log("lootbox", lootbox);
-
-  // const lootBoxPrice = lootbox.price;
-
-  const sellPriceMap = {
-    COMMON: 50,
-    UNCOMMON: 100,
-    EPIC: 200,
-    LEGENDARY: 250,
-  };
-
-  const rarityPercentage = sellPriceMap[rarity.toUpperCase()] || 0;
-
-  return rarityPercentage;
-});
-
 export default itemSchema;
